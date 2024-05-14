a1396d6b395e883a69783504d0af0e2ce58c43e0
GregoryG8 <79380668+GregoryG8@users.noreply.github.com>
2024-05-14 00:21:08 -0500

    curso php, inicios y crud

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+// print_r(PDO::getAvailableDrivers());
+
+
+
+
+// <<<Frase
+//     no cuentes los dias, haz que los dias cuenten
+// Frase;
+
+// strpos buscar
+// substr() extraer
+
+//
+
+// Imprimir
+// print "Hola a todos";
+// echo "Hola mundo", 'Hola';
+
+
+// Varibales
+// $nombre="string";
+// $edad = 10;
+// $promedio = 9.5;
+// $esAlumno = true;
+// $Nombre;
+
+// //constante
+// const escuela = "ninos";
+// define("direccion","avenida alegria");
+
+// echo gettype($nombre);
+// echo intval($promedio); fuerza el valor a ser entero
+// echo floatval($promedio);
